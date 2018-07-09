51b5573939e0bd644b39256be594defea9b5c54f
Gopal Goel <gogoe@microsoft.com>
2018-07-09 17:25:50 +0530

    Add link to open files

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,6 @@ import { initializeIcons } from '@uifabric/icons';
 // Register icons and pull the fonts from the default SharePoint cdn:
 initializeIcons();
 
-
-// import * as React from 'react';
-// import * as ReactDOM from 'react-dom';
 import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
 import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
 import { PrimaryButton } from 'office-ui-fabric-react/lib/Button';
@@ -31,18 +28,38 @@ import { Link } from 'office-ui-fabric-react/lib/Link';
 import { authContext } from './adalConfig';
 import { adalApiFetch } from './adalConfig';
 
-// console.log(authContext);
 
-const dateToFormat = '1976-04-19T12:59-0500';
 
 const columns = [
+  { 
+    key: "icon", 
+    name: "...", 
+    fieldName: "icon", 
+    minWidth: 20, 
+    maxWidth: 20,
+    isResizable: true,  
+    // onRender: (item) => (
+    //   <img src={item.icon}/>
+    // )
+    onRender: (item) => {
+      if(item.icon == ''){
+        return <i className="ms-Icon ms-Icon--FabricFolderFill" aria-hidden="true"></i>
+      }
+      else{
+        return <img src={item.icon}/>
+      }
+    }
+  },
   { 
     key: "name", 
     name: "Name", 
     fieldName: "name", 
     minWidth: 20, 
     maxWidth: 300,
-    isResizable: true  
+    isResizable: true,
+    onRender: (item) => (
+      <Link href={item.url} target='_blank' className="ms-font-m" style={{textDecoration:'none', color: 'black'}}>{item.name}</Link>
+    )
   },
   { 
     key: "position", 
@@ -86,7 +103,31 @@ const columns = [
   }
 ];
              
-
+const fileIcons: { name: string }[] = [
+  { name: 'accdb' },
+  { name: 'csv' },
+  { name: 'docx' },
+  { name: 'dotx' },
+  { name: 'mpp' },
+  { name: 'mpt' },
+  { name: 'odp' },
+  { name: 'ods' },
+  { name: 'odt' },
+  { name: 'one' },
+  { name: 'onepkg' },
+  { name: 'onetoc' },
+  { name: 'potx' },
+  { name: 'ppsx' },
+  { name: 'pptx' },
+  { name: 'pub' },
+  { name: 'vsdx' },
+  { name: 'vssx' },
+  { name: 'vstx' },
+  { name: 'xls' },
+  { name: 'xlsx' },
+  { name: 'xltx' },
+  { name: 'xsn' }
+];
 
 // console.log(items);
 
@@ -99,14 +140,12 @@ class App extends React.Component<{},{items: Array<any>}> {
     };
 
 
-    adalApiFetch(fetch, 'https://graph.microsoft.com/v1.0/me/drive/root/children', {})
-      .then((response) => {
+    adalApiFetch(fetch, 'https://graph.microsoft.com/v1.0/me/drive/root/children', {}).then((response) => {
         // console.log(response);
         // This is where you deal with your API response. In this case, we            
         // interpret the response as JSON, and then call `setState` with the
         // pretty-printed JSON-stringified object.
-        response.json()
-          .then((response) => {
+        response.json().then((response) => {
             // console.log(responseJson.body);
             console.log(JSON.stringify(response, null, 2));
             for (let i = 0; i < response.value.length; i++) { 
@@ -123,33 +162,61 @@ class App extends React.Component<{},{items: Array<any>}> {
                 size = size.toFixed(2) + " KB"
               }
               let item = {
+                icon: '',
                 key: 'item' + i,
                 name: value.name,
                 position: d,
                 office: value.lastModifiedBy.user.displayName,
                 another: size,
-                index: i
+                index: i,
+                url: ''
               };
+
+              let icon = '';
+              let name = value.name;
+              for (let i=0; i< fileIcons.length ; i++){
+                if(name.endsWith('.' + fileIcons[i].name)){
+                  icon = `https://static2.sharepointonline.com/files/fabric/assets/brand-icons/document/svg/${fileIcons[i].name}_16x1.svg`;
+                }
+              }
+              if(icon == ''){
+                  // icon = `https://static2.sharepointonline.com/files/fabric/assets/brand-icons/product/svg/onedrive_16x1.svg`;
+                  
+              }
+              item.icon = icon;
+              item.url = value.webUrl;
               this.setState((prevState) => ({
                 items: prevState.items.concat(item)
               }));
               // this.state.items.push();
             }
-          });
-      })
-      .catch((error) => {
-
+        });
+      }).catch((error) => {
         // Don't forget to handle errors!
         console.error(error);
       });
-      
+
+    // this.clickHandler = new Selection({
+    //   onSelectionChanged: () => {
+    //     this.setState({
+    //       selectionDetails: this._getSelectionDetails(),
+    //       isModalSelection: this._selection.isModal()
+    //     });
+    //   }
+    // });
+  
   }
+
   public render() {
     return (
         <div>
           <link rel="stylesheet" href="https://static2.sharepointonline.com/files/fabric/office-ui-fabric-core/9.0.0/css/fabric.min.css"/>
-          <h1 className="ComponentPage-subHeading">Files</h1>
-          <DetailsList items={ this.state.items } columns={ columns } selectionMode= {SelectionMode.none}/>
+          <p className="ms-font-su">OneDrive</p>
+          <DetailsList 
+            items={ this.state.items }
+            columns={ columns }
+            selectionMode= {SelectionMode.none}
+          />
         </div>
     );
   }
