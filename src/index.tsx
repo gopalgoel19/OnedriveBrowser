d6eab3d15da45969995e9fff9ff38edd1c1ca564
Gopal Goel <gogoe@microsoft.com>
2018-07-11 10:03:24 +0530

    Check build/public folder deployment issue

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import { authContext } from './adalConfig';
 
 const DO_NOT_LOGIN = false;
 
+document.getElementById('hello').textContent = "";
+
 runWithAdal(authContext, () => {
 
   // eslint-disable-next-line
