2ba606c442b4b8973022bc8c3e024e28ea9c50d3
Gopal Goel <gogoe@microsoft.com>
2018-08-09 13:45:51 +0530

    Remove useless comments and flies

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,5 @@ import { authContext } from './adalConfig';
 const DO_NOT_LOGIN = false;
 
 runWithAdal(authContext, () => {
-
-  // eslint-disable-next-line
-  // console.log(authContext);
   require('./indexApp.tsx');
-
 },DO_NOT_LOGIN);
