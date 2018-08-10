851c16769a78c55f0574053c1550b7f32b2a3e25
Gopal Goel <gogoe@microsoft.com>
2018-08-10 16:32:59 +0530

    Add ItemsList a separate file

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,14 @@ import {
 import { initializeIcons } from '@uifabric/icons';
 import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
 import {
-  DetailsList,
   Selection,
-  SelectionMode
 } from 'office-ui-fabric-react/lib/DetailsList';
 import { Link } from 'office-ui-fabric-react/lib/Link';
 import { authContext } from './adalConfig';
 import { adalApiFetch } from './adalConfig';
 import { Breadcrumb } from 'office-ui-fabric-react/lib/Breadcrumb';
-import { HoverCard, IExpandingCardProps } from 'office-ui-fabric-react/lib/HoverCard';
-import { IColumn } from 'office-ui-fabric-react/lib/DetailsList';
 import { fileIcons } from './fileicons';
+import { ItemsList } from './components/ItemsList';
 // Register icons and pull the fonts from the default SharePoint cdn:
 initializeIcons();
 
@@ -88,7 +85,7 @@ interface Users {
 }
 
 class App extends React.Component<{},{items: Array<any>, folders: Array<any>, users: Users}> {
-  private _selection: Selection;
+  public _selection: Selection;
 
   updateNavList: any = (obj) => {
     this.setState((prevState) => {
@@ -210,10 +207,6 @@ class App extends React.Component<{},{items: Array<any>, folders: Array<any>, us
       });
   }
 
-  navLinkUpdate: any = (props) => {
-    console.log(props);
-  }
-
   constructor(props){
     super(props);
     this.state = {
@@ -254,78 +247,15 @@ class App extends React.Component<{},{items: Array<any>, folders: Array<any>, us
           items={this.state.folders}
           ariaLabel={'Website breadcrumb'}
           />
-          <DetailsList 
+          <ItemsList 
             items={ this.state.items }
             columns={ columns }
-            selectionMode= {SelectionMode.none}
             selection={this._selection}
-            onRenderItemColumn={this.onRenderItemColumn}
+            users={this.state.users}
           />
         </div>
     );
   }
-
-  private onRenderItemColumn: any = (item: any, index: number, column: IColumn) => {
-    const expandingCardProps: IExpandingCardProps = {
-      onRenderCompactCard: this._onRenderCompactCard,
-      onRenderExpandedCard: this._onRenderExpandedCard,
-      renderData: item
-    };
-    if (column.key == 'office') {
-      return (
-        <HoverCard id="myID1" expandingCardProps={expandingCardProps} instantOpenOnClick={true}>
-          <div className="HoverCard-item" data-is-focusable={true}>
-            {item.office}
-          </div>
-        </HoverCard>
-      );
-    }
-    return item[column.key];
-  };
-
-  private _onRenderCompactCard = (item: any): JSX.Element => {
-    let id = item.value.lastModifiedBy.user.id;
-    let user = this.state.users[id];
-    return (
-      <div className="hoverCardExample-compactCard">
-
-      <span style={{display: 'inline-block', width: '140px', height: 'auto'}}>
-          <img aria-hidden="true" src={user.imageUrl}
-          style={{display: 'inline', width: '100%', height: 'auto', padding: '10px', borderRadius: '50%'}}/>
-      </span>
-      <span style={{display: 'inline-block', padding: '0px'}} >
-          
-          <div className="hoverCardExample-expandedCard" style={{margin: '10px'}}>
-            <div>
-              <span className="ms-Icon ms-Icon--Contact" aria-hidden="true" style={{padding: '2px'}}></span><span>{user.displayName}</span>
-            </div>
-            <div>
-              <span className="ms-Icon ms-Icon--Education" aria-hidden="true" style={{padding: '2px'}}></span><span>{user.jobTitle}</span>
-            </div>     
-          </div>
-      </span>
-      </div>
-    );
-  };
-
-  private _onRenderExpandedCard = (item: any): JSX.Element => {
-    let id = item.value.lastModifiedBy.user.id;
-    let user = this.state.users[id];
-    return (
-      <div className="hoverCardExample-expandedCard" style={{margin: '10px'}}>
-        <div className='ms-font-su'>Contact</div>
-        <div style={{padding: '2px'}}>
-          <span className="ms-Icon ms-Icon--MailSolid" aria-hidden="true" style={{padding: '5px'}}></span><span className='ms-font-m'> {user.mail}</span>
-        </div>
-        <div style={{padding: '2px'}}>
-          <span className="ms-Icon ms-Icon--Location" aria-hidden="true" style={{padding: '5px'}}></span><span className='ms-font-m'> {user.officeLocation}</span>
-        </div>
-        <div style={{padding: '2px'}}>
-          <span className="ms-Icon ms-Icon--Phone" aria-hidden="true" style={{padding: '5px'}}></span><span className='ms-font-m'> {user.businessPhones}</span>
-        </div>      
-      </div>
-    );
-  };
 }
 
 export default App;
---
+++ src/components/ItemsList.tsx
@@ -0,0 +1,168 @@
+
+import * as React from 'react';
+import {
+  DocumentCard,
+  DocumentCardActivity,
+  DocumentCardPreview,
+  DocumentCardTitle
+} from 'office-ui-fabric-react/lib/DocumentCard';
+import { initializeIcons } from '@uifabric/icons';
+import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
+import {
+  DetailsList,
+  Selection,
+  SelectionMode
+} from 'office-ui-fabric-react/lib/DetailsList';
+import { Link } from 'office-ui-fabric-react/lib/Link';
+import { authContext } from './../adalConfig';
+import { adalApiFetch } from './../adalConfig';
+import { Breadcrumb } from 'office-ui-fabric-react/lib/Breadcrumb';
+import { HoverCard, IExpandingCardProps } from 'office-ui-fabric-react/lib/HoverCard';
+import { IColumn } from 'office-ui-fabric-react/lib/DetailsList';
+import { fileIcons } from './../fileicons';
+// Register icons and pull the fonts from the default SharePoint cdn:
+initializeIcons();
+
+
+const columns = [
+  { 
+    key: "icon", 
+    name: "...", 
+    fieldName: "icon", 
+    minWidth: 20, 
+    maxWidth: 20,
+    isResizable: false,  
+    onRender: (item) => {
+      if(item.icon == ''){
+        if('file' in item.value) return <i className="ms-Icon ms-Icon--FileTemplate" aria-hidden="true"></i>
+        else return <i className="ms-Icon ms-Icon--FabricFolderFill" aria-hidden="true"></i>
+      }
+      else{
+        return <img src={item.icon}/>
+      }
+    }
+  },
+  { 
+    key: "name", 
+    name: "Name", 
+    fieldName: "name", 
+    minWidth: 20, 
+    maxWidth: 300,
+    isResizable: true,
+    onRender: (item) => {
+      if('folder' in item.value){
+        return <Link href='' className="ms-font-m" style={{textDecoration:'none', color: 'black'}}>{item.name}</Link>
+      }
+      else{
+        return <Link href={item.url} target='_blank' className="ms-font-m" style={{textDecoration:'none', color: 'black'}}>{item.name}</Link>
+      }
+    }
+  },
+  { 
+    key: "modified", 
+    name: "Modified",
+    fieldName: "modified",
+    minWidth: 20, 
+    maxWidth: 300,
+    isResizable: true
+  },
+  { 
+    key: "modifiedBy", 
+    name: "Modified By",
+    fieldName: "modifiedBy",
+    minWidth: 160, 
+    maxWidth: 280,
+    isResizable: true       
+  },
+  {
+    key: "size",
+    name: 'Size',
+    fieldName: "size",
+    minWidth: 70,
+    maxWidth: 100
+  }
+];
+
+interface Users {  
+    id: object;
+}
+
+export class ItemsList extends React.Component<{items: Array<any>, selection: Selection, users: Users, columns: any},{}> {
+
+
+  public render() {
+    return (
+      <DetailsList 
+        items={ this.props.items }
+        columns={ columns }
+        selectionMode= {SelectionMode.none}
+        selection={this.props.selection}
+        onRenderItemColumn={this.onRenderItemColumn}
+      />
+    );
+  }
+
+  private onRenderItemColumn: any = (item: any, index: number, column: IColumn) => {
+    const expandingCardProps: IExpandingCardProps = {
+      onRenderCompactCard: this._onRenderCompactCard,
+      onRenderExpandedCard: this._onRenderExpandedCard,
+      renderData: item
+    };
+    if (column.key == 'office') {
+      return (
+        <HoverCard id="myID1" expandingCardProps={expandingCardProps} instantOpenOnClick={true}>
+          <div className="HoverCard-item" data-is-focusable={true}>
+            {item.office}
+          </div>
+        </HoverCard>
+      );
+    }
+    return item[column.key];
+  };
+
+  private _onRenderCompactCard = (item: any): JSX.Element => {
+    let id = item.value.lastModifiedBy.user.id;
+    let user = this.props.users[id];
+    return (
+      <div className="hoverCardExample-compactCard">
+
+      <span style={{display: 'inline-block', width: '140px', height: 'auto'}}>
+          <img aria-hidden="true" src={user.imageUrl}
+          style={{display: 'inline', width: '100%', height: 'auto', padding: '10px', borderRadius: '50%'}}/>
+      </span>
+      <span style={{display: 'inline-block', padding: '0px'}} >
+          
+          <div className="hoverCardExample-expandedCard" style={{margin: '10px'}}>
+            <div>
+              <span className="ms-Icon ms-Icon--Contact" aria-hidden="true" style={{padding: '2px'}}></span><span>{user.displayName}</span>
+            </div>
+            <div>
+              <span className="ms-Icon ms-Icon--Education" aria-hidden="true" style={{padding: '2px'}}></span><span>{user.jobTitle}</span>
+            </div>     
+          </div>
+      </span>
+      </div>
+    );
+  };
+
+  private _onRenderExpandedCard = (item: any): JSX.Element => {
+    let id = item.value.lastModifiedBy.user.id;
+    let user = this.props.users[id];
+    return (
+      <div className="hoverCardExample-expandedCard" style={{margin: '10px'}}>
+        <div className='ms-font-su'>Contact</div>
+        <div style={{padding: '2px'}}>
+          <span className="ms-Icon ms-Icon--MailSolid" aria-hidden="true" style={{padding: '5px'}}></span><span className='ms-font-m'> {user.mail}</span>
+        </div>
+        <div style={{padding: '2px'}}>
+          <span className="ms-Icon ms-Icon--Location" aria-hidden="true" style={{padding: '5px'}}></span><span className='ms-font-m'> {user.officeLocation}</span>
+        </div>
+        <div style={{padding: '2px'}}>
+          <span className="ms-Icon ms-Icon--Phone" aria-hidden="true" style={{padding: '5px'}}></span><span className='ms-font-m'> {user.businessPhones}</span>
+        </div>      
+      </div>
+    );
+  };
+}
+
+// export default ItemsList;
