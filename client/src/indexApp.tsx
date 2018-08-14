54c98ae3a0ea849705f3d1d561e91c4c159de7c8
Gopal Goel <gogoe@microsoft.com>
2018-08-14 12:04:59 +0530

    Setup apollo client

--- client/src/indexApp.tsx
+++ client/src/indexApp.tsx
@@ -3,6 +3,25 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import gql from "graphql-tag";
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000"
+});
+
+
+client
+  .query({
+    query: gql`
+      {
+        books{
+          title
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 ReactDOM.render(
   <App />,
